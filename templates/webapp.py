2b9d445b9c6501e2bd04945f485c43e540d06e8d
40282640 <andrewprow13@gmail.com>
2016-11-02 17:39:03 +0000

    Set09103

---
+++ templates/webapp.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+  return render_template('templates/index.html'), 200
+
+
+@app.route('/about/')
+def about():
+    return render_template('about1.html')
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0' , debug = True) 
+
